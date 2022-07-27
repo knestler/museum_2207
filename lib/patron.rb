78b6e27e6c63953284a8399c571a4bacd0a2ddda
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-27 12:43:57 -0400

    add patron class and fix spec complete patron

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,4 +1,19 @@
 class Patron
 
+attr_reader :name, :spending_money
+
+  def initialize (name, spending_money)
+    @name = name
+    @spending_money = spending_money
+    @interests = []
+  end
+
+  def interests
+    @interests
+  end
+
+  def add_interest(interested_in)
+    @interests << interested_in
+  end
 
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -5,21 +5,21 @@ require './lib/patron.rb'
 RSpec.describe Patron do
 
   before :each do
-    patron_1 = Patron.new("Bob", 20)
+    @patron_1 = Patron.new("Bob", 20)
   end
 
   it 'has attributes' do
-    expect(patron_1.name).to eq("Bob")
-    expect(patron_1.spending_money).to eq(20)
+    expect(@patron_1.name).to eq("Bob")
+    expect(@patron_1.spending_money).to eq(20)
   end
 
   it 'has interests' do
-    expect(patron_1.interests).to eq[]
+    expect(@patron_1.interests).to eq([])
+    
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
 
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
-
-    expect(patron_1.interests).to eq("Dead Sea Scrolls", "Gems and Minerals")
+    expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 
 

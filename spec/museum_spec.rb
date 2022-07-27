499b4a50af784f9f9bfeb360c233dff1f58a6eed
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-27 13:42:10 -0400

    add tests to museum spec

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -4,5 +4,37 @@ require './lib/museum.rb'
 
 RSpec.describe Museum do
 
+  before :each do
+    @dmns = Museum.new("Denver Museum of Nature and Science")
 
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+  end
+
+  it 'has attributes' do
+    expect(@dmns.name).to eq("Denver Museum of Nature and Science")
+    expect(@dmns.exhibits).to eq([])
+  end
+    
+  it 'has exhibits' do
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+  end 
+
+  it 'recommends exhibits to patrons' do
+    patron_1 = Patron.new("Bob", 20) 
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    expect(@dmns.recommend_exhibits(patron_1)).to eq([@dead_sea_scrolls, @gems_and_minerals])
+    expect(@dmns.recommend_exhibits(patron_2)).to eq([@imax])
+
+  end 
 end 
